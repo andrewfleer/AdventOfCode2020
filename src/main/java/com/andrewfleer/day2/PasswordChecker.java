64d6b79197757992b5b9de0f501340e0bda67e4e
Andrew <andrew.fleer@gmail.com>
2020-12-02 23:19:12 -0600

    Day 2 Part 2

--- src/main/java/com/andrewfleer/day2/PasswordChecker.java
+++ src/main/java/com/andrewfleer/day2/PasswordChecker.java
@@ -14,7 +14,8 @@ public class PasswordChecker {
         try {
             List inputs = fileUtil.readFileToList(fileName);
 
-            int validPasswords = findValidPasswords(inputs);
+            //int validPasswords = findValidPasswordsPart1(inputs);
+            int validPasswords = findValidPasswordsPart2(inputs);
 
             System.out.println(validPasswords);
 
@@ -23,7 +24,7 @@ public class PasswordChecker {
         }
     }
 
-    private static int findValidPasswords(List<String> inputs) {
+    private static int findValidPasswordsPart1(List<String> inputs) {
         int validPasswords = 0;
         for (String input : inputs) {
             String[] params = input.split(" ");
@@ -34,7 +35,7 @@ public class PasswordChecker {
             String[] minAndMax = requirements.split("-");
             letter = letter.substring(0,1);
 
-            if (isPasswordValid(letter, password, minAndMax)) {
+            if (isPasswordValidPart1(letter, password, minAndMax)) {
                 validPasswords++;
             }
         }
@@ -42,7 +43,7 @@ public class PasswordChecker {
         return validPasswords;
     }
 
-    public static boolean isPasswordValid(String letter, String password, String[] minAndMax) {
+    public static boolean isPasswordValidPart1(String letter, String password, String[] minAndMax) {
         boolean passwordValid = false;
         int min = Integer.parseInt(minAndMax[0]);
         int max = Integer.parseInt(minAndMax[1]);
@@ -52,6 +53,48 @@ public class PasswordChecker {
         if (count >= min && count <= max) {
             passwordValid = true;
         }
+
+        return passwordValid;
+    }
+
+    private static int findValidPasswordsPart2(List<String> inputs) {
+        int validPasswords = 0;
+        for (String input : inputs) {
+            String[] params = input.split(" ");
+            String requirements = params[0];
+            String letter = params[1];
+            String password = params[2];
+
+            String[] positions = requirements.split("-");
+            letter = letter.substring(0,1);
+
+            if (isPasswordValidPart2(letter, password, positions)) {
+                validPasswords++;
+            }
+        }
+
+        return validPasswords;
+    }
+
+    public static boolean isPasswordValidPart2(String letter, String password, String[] positions) {
+        boolean passwordValid = false;
+        int firstPosition = Integer.parseInt(positions[0]);
+        int secondPosition = Integer.parseInt(positions[1]);
+        char character = letter.charAt(0);
+
+        // First make sure the password is long enough
+        // If not, assume false;
+        if (password.length() < secondPosition) {
+            return passwordValid;
+        }
+
+        // Have to subtract 1, because the input is not zero-indexed
+        // also ^ is an XOR operator
+        if ((password.charAt(firstPosition - 1) == character) ^
+                (password.charAt(secondPosition - 1) == character)) {
+            passwordValid = true;
+        }
+
 
         return passwordValid;
     }
--- src/test/java/day2/PasswordCheckerTest.java
+++ src/test/java/day2/PasswordCheckerTest.java
@@ -19,22 +19,58 @@ public class PasswordCheckerTest {
 
 
     @Test
-    public void testPasswordValidPasswordRequirement() {
-        boolean passwordValid = passwordChecker.isPasswordValid(a,password,minAndMax);
+    public void testPasswordValidPasswordRequirementPart1() {
+        boolean passwordValid = passwordChecker.isPasswordValidPart1(a,password,minAndMax);
 
         assert(passwordValid);
     }
 
     @Test
-    public void testPasswordRequirementNotEnough() {
-        boolean passwordValid = passwordChecker.isPasswordValid(f, password, minAndMax);
+    public void testPasswordRequirementPart1NotEnough() {
+        boolean passwordValid = passwordChecker.isPasswordValidPart1(f, password, minAndMax);
 
         assert(!passwordValid);
     }
 
     @Test
-    public void testPasswordRequirementTooMany() {
-        boolean passwordValid = passwordChecker.isPasswordValid(s, password, minAndMax);
+    public void testPasswordRequirementPart1TooMany() {
+        boolean passwordValid = passwordChecker.isPasswordValidPart1(s, password, minAndMax);
+
+        assert(!passwordValid);
+    }
+
+    @Test
+    public void testPasswordRequirementPart2PassFirstPosition() {
+        String[] positions = new String[] {"1", "5"};
+
+        boolean passwordValid = passwordChecker.isPasswordValidPart2(s, password, positions);
+
+        assert(passwordValid);
+    }
+
+    @Test
+    public void testPasswordRequirementPart2PassSecondPosition() {
+        String[] positions = new String[] {"1", "5"};
+
+        boolean passwordValid = passwordChecker.isPasswordValidPart2(a, password, positions);
+
+        assert(passwordValid);
+    }
+
+    @Test
+    public void testPasswordRequirementPart2FailBothPositions() {
+        String[] positions = new String[] {"1", "4"};
+
+        boolean passwordValid = passwordChecker.isPasswordValidPart2(s, password, positions);
+
+        assert(!passwordValid);
+    }
+
+    @Test
+    public void testPasswordRequirementPart2FailNoPositions() {
+        String[] positions = new String[] {"1", "4"};
+
+        boolean passwordValid = passwordChecker.isPasswordValidPart2(f, password, positions);
 
         assert(!passwordValid);
     }
